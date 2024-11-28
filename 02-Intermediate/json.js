71738eb7792d26148b2b458611758a92ccb5c35f
Hanzala <Hanzala.hc@gmail.com>
2024-11-28 21:23:17 +0500

    destructure of object

---
+++ 02-Intermediate/json.js
@@ -0,0 +1,15 @@
+// 09 - Json & Destructor
+
+const course = {
+  title: "JavaScript - The Complete Guide",
+  price: 199,
+  description: "Learn JavaScript from scratch",
+};
+
+const { title, price, description: desc } = course;
+
+console.log(title, price, desc);
+
+// api concept
+
+// https://api.github.com/users/Hanzalahc
--- 02-Intermediate/objectPartTwo.js
+++ 02-Intermediate/objectPartTwo.js
@@ -1,2 +1,32 @@
 // 07 - Objects
 
+const userInfo = {
+  name: "John",
+  age: 30,
+  isAdmin: true,
+};
+
+const formatedUserInfo = {
+  id: 1,
+};
+
+const user = {
+  ...formatedUserInfo,
+  ...userInfo,
+};
+
+// console.log(user);
+
+// Array of objects
+
+const users = [
+  { name: "John", age: 30 },
+  { name: "Jane", age: 25 },
+  { name: "Jack", age: 35 },
+];
+
+console.log(users);
+console.log("Keys: ", Object.keys(user));
+console.log("Values: ", Object.values(user));
+console.log("Entries: ", Object.entries(user)); // Returns an array of arrays
+console.log("has Porpery: ", user.hasOwnProperty("name"));
